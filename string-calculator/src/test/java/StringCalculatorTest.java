1dc23d5d6c1cfa3ca54bdedf74272e2ee3760250
YogeshwarPerumal <yogeshwar1997@hotmail.com>
2024-12-29 19:05:49 +0530

    add test case for negative numbers in input

--- string-calculator/src/test/java/StringCalculatorTest.java
+++ string-calculator/src/test/java/StringCalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTest {
 
@@ -47,4 +48,11 @@ public class StringCalculatorTest {
     public void testSpecialCharacterDelimiter() {
         assertEquals(stringCalculator.add("//$\n1$2\n\n$3$4\n\n\n\n"), 10);
     }
+
+    @Test
+    public void testNegativeNumbers() {
+        Exception exception = assertThrows(Exception.class, () -> stringCalculator.add("//;\n1;2;\n-3"));
+        assertEquals("Negative numbers are not allowed -3", exception.getMessage());
+    }
+
 }
